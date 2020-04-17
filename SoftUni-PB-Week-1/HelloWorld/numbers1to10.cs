9710ce641bc3253bad8a23f8940147206bdd0bf8
shllshckd <52105864+shllshckd@users.noreply.github.com>
2020-04-17 21:07:05 +0300

    upload

---
+++ SoftUni-PB-Week-1/HelloWorld/numbers1to10.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace uprajnenie
+{
+    class numbers1to10
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            Console.WriteLine(num);
+            num = num + 1;
+            Console.WriteLine(num);
+            num = num + 1;
+            Console.WriteLine(num);
+            num = num + 1;
+            Console.WriteLine(num);
+            num = num + 1;
+            Console.WriteLine(num);
+            num = num + 1;
+            Console.WriteLine(num);
+            num = num + 1;
+            Console.WriteLine(num);
+            num = num + 1;
+            Console.WriteLine(num);
+            num = num + 1;
+            Console.WriteLine(num);
+            num = num + 1;
+            Console.WriteLine(num);
+        }
+    }
+}
---
+++ SoftUni-PB-Week-1/numbers1to10-SimpleForLoop/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace numbers1to10SimpleForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 11; ++i)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
